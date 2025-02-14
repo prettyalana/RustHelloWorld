3654747652329b2cb3efae30a6202fe155fe83c4
prettyalana <129349619+prettyalana@users.noreply.github.com>
2025-02-14 14:14:29 -0600

    Create false program and false test - Exit with any value between 1 and 255 to indicate an error - Test false to verify that the program reports a failure when run

--- tests/cli.rs
+++ tests/cli.rs
@@ -2,8 +2,14 @@ use assert_cmd::Command;
 
 #[test] // unit test function marker 
 fn true_ok() {
-    let mut cmd = Command::cargo_bin("true").unwrap();
+    let mut cmd = Command::cargo_bin("true").unwrap(); // unwrap will cause a panic and the test will fail
     cmd.assert().success();
+}
+
+#[test]
+fn false_not_ok() {
+    let mut cmd = Command::cargo_bin("false").unwrap();
+    cmd.assert().failure();
 }
 
 #[test]
