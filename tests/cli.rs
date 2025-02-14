60545eb3d1d2183efa7f28c70d64be13b07e9660
prettyalana <129349619+prettyalana@users.noreply.github.com>
2025-02-13 18:38:37 -0600

    Test true.rs

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,6 +1,12 @@
 use assert_cmd::Command;
 
 #[test]
+fn true_ok() {
+    let mut cmd = Command::cargo_bin("true").unwrap();
+    cmd.assert().success();
+}
+
+#[test]
 fn runs() {
     let mut cmd = Command::cargo_bin("hello").unwrap();
     cmd.assert().success();
