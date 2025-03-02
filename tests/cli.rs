5f5ac3712f0a7e159dac453e1fa3990d963c6794
prettyalana <129349619+prettyalana@users.noreply.github.com>
2025-03-01 22:25:07 -0600

    Update runs test and test the program output - Import the pretty_assertions::assert_eq macro for comparing values - Execute the hello command - Use Result::expect to get the output of the command or a failure message stating fail - Verify the command succeeded - Covert the output to UTF-8 - Compare the output from the program to an expected value using the pretty_assertions version of the assert_eq macro

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,4 +1,5 @@
 use assert_cmd::Command;
+use pretty_assertions::assert_eq;
 
 #[test] // unit test function marker 
 fn true_ok() {
@@ -15,5 +16,8 @@ fn false_not_ok() {
 #[test]
 fn runs() {
     let mut cmd = Command::cargo_bin("hello").unwrap();
-    cmd.assert().success();
+    let output = cmd.output().expect("fail");
+    assert!(output.status.success());
+    let stdout = String::from_utf8(output.stdout).expect("invalid UTF-8");
+    assert_eq!(stdout, "Hello, world!\n");
 }
